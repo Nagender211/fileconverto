1a2ce90830e72d597f7b7f016e26a5f2415bf9bc
Nagender211 <dnagender2019@gmail.com>
2024-04-05 23:14:54 +0530

    added the files

---
+++ server/database/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DBConnection = async () => {
+    const USERNAME = process.env.DB_USERNAME;
+    const PASSWORD = process.env.DB_PASSWORD;
+    
+
+    const MONGO_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@fileshare.h722gtd.mongodb.net/?retryWrites=true&w=majority&appName=fileshare`;
+    try {
+        await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+
+export default DBConnection;
